83fe76679704433d1e723fb729f0a6a2a20fba81
zbeyens <ziad.beyens@borealisgroup.com>
2019-04-27 16:51:14 +0200

    space and shadows

--- packages/theme/src/generate.js
+++ packages/theme/src/generate.js
@@ -12,18 +12,15 @@ const borderRadiuses = [
   `${baseBorderRadius * 4}${PX}`,
   '50%',
 ];
-const getNumberSequence = n => Array.from(Array(n).keys());
-const space = getNumberSequence(8).map(base => {
-  let result = base;
-  if (base) result = 2 ** base;
-  return result;
-});
+
+const space = [0, 4, 8, 16, 24, 32, 40, 48, 64, 80, 96, 112, 128];
 
 const shadows = [
   '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
   '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
   '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
   '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)',
+  '0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22)',
 ];
 
 const breakPoints = [
