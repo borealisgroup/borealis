cd55c9ca1a08f03e549815da7c1211f5ca0f5a8a
Lennert Van Sever <lennert.vansever@borealisgroup.com>
2019-04-29 13:23:52 +0200

    os-2 making theme build properly

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   presets: [['@babel/preset-env']],
-  plugins: [["@babel/plugin-proposal-object-rest-spread"]]
+  plugins: [
+    ['@babel/plugin-proposal-object-rest-spread'],
+    ['inline-json-import', {}],
+  ],
 };
