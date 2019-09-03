7458da6acfcbaa7c3b40eb50244cb4bdc7fac94c
zbeyens <ziad.beyens@udemus.org>
2019-09-03 12:50:35 +0200

    chore: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,9 @@
-module.exports = require('./packages/eslint-config');
+module.exports = {
+  extends: '@borealisgroup/eslint-config',
+  rules: {
+    'import/no-dynamic-require': 'off',
+    'global-require': 'off',
+    'no-multi-assign': 'off',
+    'no-await-in-loop': 'off'
+  }
+};
