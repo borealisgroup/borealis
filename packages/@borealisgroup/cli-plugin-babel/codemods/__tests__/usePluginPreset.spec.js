f160281ad8f61c542510473d36fa43fbb73dd441
test <test@test.com>
2019-10-03 22:20:57 +0200

    init

--- packages/@borealisgroup/cli-plugin-babel/codemods/__tests__/usePluginPreset.spec.js
+++ packages/@borealisgroup/cli-plugin-babel/codemods/__tests__/usePluginPreset.spec.js
@@ -1,9 +1,8 @@
-jest.autoMockOff()
+jest.autoMockOff();
 
-const { defineTest } = require('jscodeshift/dist/testUtils')
+const { defineTest } = require('jscodeshift/dist/testUtils');
 
-defineTest(__dirname, 'usePluginPreset', null, 'default')
-defineTest(__dirname, 'usePluginPreset', null, 'customConfig')
-defineTest(__dirname, 'usePluginPreset', null, 'require')
-defineTest(__dirname, 'usePluginPreset', null, 'templateLiteral')
-
+defineTest(__dirname, 'usePluginPreset', null, 'default');
+defineTest(__dirname, 'usePluginPreset', null, 'customConfig');
+defineTest(__dirname, 'usePluginPreset', null, 'require');
+defineTest(__dirname, 'usePluginPreset', null, 'templateLiteral');
