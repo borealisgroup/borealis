8f930065b612e3af8e1b17676aeaa65f0ce30c46
test <test@test.com>
2019-10-15 18:25:03 +0200

    feat: prompt modules

--- packages/@borealisgroup/cli-config/promptModules/index.js
+++ packages/@borealisgroup/cli-config/promptModules/index.js
@@ -4,14 +4,15 @@
  */
 exports.getPromptModules = () => {
   return [
-    'babel',
-    'typescript',
-    'pwa',
-    'router',
-    'vuex',
-    'cssPreprocessors',
-    'linter',
-    'unit',
-    'e2e',
+    'vue-core',
+    'cra',
+    // 'typescript',
+    // 'pwa',
+    // 'router',
+    // 'vuex',
+    // 'cssPreprocessors',
+    // 'linter',
+    // 'unit',
+    // 'e2e',
   ].map(file => require(`../promptModules/${file}`));
 };
