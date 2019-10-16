2bb53b0d08c24b94bdd69494030826ff81647448
test <test@test.com>
2019-10-16 16:12:33 +0200

    feat: cli add

--- packages/@borealisgroup/cli/bin/cli.js
+++ packages/@borealisgroup/cli/bin/cli.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+// TODO: conditions on string
+require('dotenv').config();
 const chalk = require('chalk');
 const minimist = require('minimist');
 const envinfo = require('envinfo');
@@ -63,6 +65,38 @@ program
     }
 
     require('../lib/create')(name, options);
+  });
+
+//
+// ─── ADD ────────────────────────────────────────────────────────────────────────
+//
+program
+  .command('add <plugin> [pluginOptions]')
+  .description(
+    'install a plugin and invoke its generator in an already created project'
+  )
+  .option(
+    '--registry <url>',
+    'Use specified npm registry when installing dependencies (only for npm)'
+  )
+  .allowUnknownOption()
+  .action(plugin => {
+    require('../lib/add')(plugin, minimist(process.argv.slice(3)));
+  });
+
+//
+// ─── INVOKE ─────────────────────────────────────────────────────────────────────
+//
+program
+  .command('invoke <plugin> [pluginOptions]')
+  .description('invoke the generator of a plugin in an already created project')
+  .option(
+    '--registry <url>',
+    'Use specified npm registry when installing dependencies (only for npm)'
+  )
+  .allowUnknownOption()
+  .action(plugin => {
+    require('../lib/invoke')(plugin, minimist(process.argv.slice(3)));
   });
 
 //
