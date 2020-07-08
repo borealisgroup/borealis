c4ef019a2de9790944c4f9b724e1a0ce76f7fa51
zbeyens <ziyad.beyens@gmail.com>
2020-07-08 17:17:33 +0200

    colors

---
+++ design-system.framerfx/code/canvas.tsx
@@ -0,0 +1,44 @@
+// WARNING: this file is auto generated, any changes will be lost
+import { createDesignComponent, CanvasStore } from "framer"
+const canvas = CanvasStore.shared(); // CANVAS_DATA;
+
+export const colors = Object.freeze({
+    /** #002D5A */
+    "primary": "var(--token-e6413486-46ec-4506-9cf6-756ec28e3436, rgb(0, 45, 90))",
+    /** #005D9A */
+    "secondary": "var(--token-a2690f35-d597-4a16-babd-1200f024fbf3, rgb(0, 93, 154))",
+    /** #0099D2 */
+    "tertiary": "var(--token-4a25f396-2e3a-45f2-9e56-a768a2f4f9c6, rgb(0, 153, 210))",
+    /** #002D5A */
+    "darkblue": "var(--token-8c3939bf-baee-4f32-8e13-41d9c45606b5, rgb(0, 45, 90))",
+    /** #005D9A */
+    "blue": "var(--token-eed3edbd-a75b-44e7-9cf6-64685d09f986, rgb(0, 93, 154))",
+    /** #D10000 */
+    "red": "var(--token-361e655c-60d0-4446-8bed-9496c8b3c6c8, rgb(209, 0, 0))",
+    /** #FAB900 */
+    "lightorange": "var(--token-662cae15-0d51-4c9d-8cb4-650afa075660, rgb(250, 185, 0))",
+    /** #FFE100 */
+    "yellow": "var(--token-5581699d-d523-4bfb-b2d7-e93cce408051, rgb(255, 225, 0))",
+    /** #91B119 */
+    "green": "var(--token-eaedb741-2344-4641-b5b0-025629a16569, rgb(145, 177, 25))",
+    /** #BFBFBF */
+    "gray": "var(--token-0d06ff61-b19e-4f0c-9ffd-aea5f042c42b, rgb(191, 191, 191))",
+    /** #FFFFFF */
+    "white": "var(--token-81b1b61c-0c57-44b5-a814-b30ac6596e3c, rgb(255, 255, 255))",
+    /** #000000 */
+    "black": "var(--token-e7d9ffd6-0d65-41d2-a6f8-18ea55d60ac2, rgb(0, 0, 0))",
+    /** rgba(0, 0, 0, 0.8) */
+    "black-title": "var(--token-18472bd8-d560-48f8-8897-0fa8d7073c2b, rgba(0, 0, 0, 0.8))",
+    /** rgba(0, 0, 0, 0.65) */
+    "black-primary": "var(--token-6cc29b36-bd37-46cc-94a2-f1dce78a7bbf, rgba(0, 0, 0, 0.65))",
+    /** rgba(0, 0, 0, 0.5) */
+    "black-secondary": "var(--token-f47c727c-b389-4feb-825e-9d0e1353a745, rgba(0, 0, 0, 0.5))",
+    /** rgba(0, 0, 0, 0.25) */
+    "black-disable": "var(--token-d6155397-a93f-4169-80e9-4db9de087e72, rgba(0, 0, 0, 0.25))",
+    /** rgba(0, 0, 0, 0.15) */
+    "black-border": "var(--token-f9269743-6e44-40ea-b285-1245510e504f, rgba(0, 0, 0, 0.15))",
+    /** rgba(0, 0, 0, 0.06) */
+    "black-dividers": "var(--token-c07fb98a-2488-461d-aa67-ddb1741f7d9c, rgba(0, 0, 0, 0.06))",
+    /** rgba(0, 0, 0, 0.04) */
+    "black-background": "var(--token-ba35c1c7-6c83-404a-a0ee-c5bbc334749b, rgba(0, 0, 0, 0.04))",
+})
