ca1d5a0b9fe6d642745ae78f42a66c9d5327aeb2
Lennert Van Sever <lennert.vansever@borealisgroup.com>
2021-08-25 15:25:07 +0200

    fix: avoid false positive no shadow with Typescript fix

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -33,11 +33,13 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/no-useless-constructor': 'error',
     '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-shadow': 'error',
 
     'babel/no-unused-expressions': 'off',
     'no-undef': 'off',
     'no-unexpected-multiline': 'off',
     'no-unused-vars': 'off',
     'no-useless-constructor': 'off',
+    'no-shadow': 'off',
   },
 };
